9d1900b4d6a3d0697d20fb77bdf33abc62315516
tamayama ryo <ryou0827@gmail.com>
2018-07-25 11:12:09 +0900

    ファースト

---
+++ 6_20_kadai/src/dao/RegStudentDAO.java
@@ -0,0 +1,74 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dto.SikakuDTO;
+
+public class RegStudentDAO {
+
+	public static SikakuDTO Registration(int studentId, String studentName, int studentAge, String studentGender){
+
+		SikakuDTO result = null;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/kadai1?useSSL=false",
+					"root",
+					"pkp12345");
+
+			String sql = "insert into sikaku values(?, ?, ?, ?);";
+
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setInt(1, studentId);
+			pstmt.setString(2, studentName);
+			pstmt.setInt(3, studentAge);
+			pstmt.setString(4, studentGender);
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException e){
+			System.out.println("DBアクセスに失敗しました。");
+			e.printStackTrace();
+		} catch (Exception e){
+			System.out.println("数字を指定してください。");
+		} finally {
+			try {
+				if( rs != null){
+					rs.close();
+				}
+			} catch(SQLException e){
+				System.out.println("DB切断時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+			try {
+				if( pstmt != null){
+					pstmt.close();
+				}
+			} catch(SQLException e){
+				System.out.println("DB切断時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+
+			try {
+				if( con != null){
+					con.close();
+				}
+			} catch (SQLException e){
+				System.out.println("DB切断時にエラーが発生しました。");
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+}
